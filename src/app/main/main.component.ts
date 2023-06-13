208e9638e8351fa975af9db9c19828d136a587dc
MagzhanJSoft <magzhanatullaev@gmail.com>
2023-06-13 11:02:17 +0500

    20230613_1100

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -54,7 +54,7 @@ export class MainComponent implements OnInit {
               }, {
                 label: 'Бюджет',
                 command: () => this.openTab('app-budjet-list', 'Бюджет', '')
-              },{
+              }, {
                 label: 'Классификации',
                 command: () => this.openTab('app-classification-income-list', 'Классификации', '')
               }]
@@ -72,7 +72,7 @@ export class MainComponent implements OnInit {
               items: [{
                 label: 'Утвержденный план по поступлениям',
                 command: () => this.openTab('app-utv-income-list', 'Утвержденный план по поступлениям', '')
-              },{
+              }, {
                 label: 'Изменения по поступлению',
                 command: () => this.openTab('app-izm-inc-doc-list', 'Изменения по поступлению', '')
               }]
